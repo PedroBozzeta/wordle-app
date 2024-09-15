4a0044a8de62b4169ab838788f55a3201a6167a7
PedroBozzeta <108239149+PedroBozzeta@users.noreply.github.com>
2024-09-15 15:37:33 -0500

    fix(InvisibleInput): Changing reference inplementation

--- src/components/InvisibleInput.tsx
+++ src/components/InvisibleInput.tsx
@@ -3,10 +3,16 @@ import { useEffect, useRef } from "react"
 
 
 const InvisibleInput = () => {
-    const inputRef= useRef<HTMLInputElement  |null>(null)
-    useEffect(() => {
+    const inputRef = useRef<HTMLInputElement | null>(null)
+    
+    const handleTap = () => {
         inputRef.current?.focus(); 
-    },[])
+    }
+    useEffect(() => {
+        window.addEventListener('click',handleTap)
+        return ()=>window.removeEventListener('click',handleTap)
+    }, [handleTap])
+    
   return (
     <input type='text' className="invisible-input" ref={inputRef}></input>
   )
