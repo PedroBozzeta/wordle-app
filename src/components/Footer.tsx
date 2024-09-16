3bb693be56d1b5b450120d823523b3bad20b74fe
PedroBozzeta <108239149+PedroBozzeta@users.noreply.github.com>
2024-09-15 21:37:53 -0500

    feat(Footer): Adding clicking funcionality on footer to restart

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,19 @@
-import { FINISHED } from "../constants/GameConstants";
+
+import { FINISHED, RESTART } from "../constants/GameConstants";
+import { ReducerAction } from "../hooks/useGameStateReducer";
 import InvisibleInput from "./InvisibleInput";
 
 interface FooterProps {
     gameState: string;
-    currentWord: string;
+  currentWord: string;
+  dispatch: (action: ReducerAction) => void
 }
 const Footer = (props: FooterProps) => {
   return (
     <div className="footer">
           <InvisibleInput currentWord={props.currentWord} gameState={props.gameState}/>
       {props.gameState == FINISHED && (
-        <h4 className="montserrat-font heartbeat press-to-restart">{`PRESS [Enter] TO RESTART`}</h4>
+        <h4 className="montserrat-font heartbeat press-to-restart" onClick={()=>props.dispatch({type:RESTART})}>{`TAP OR PRESS [Enter] TO RESTART`}</h4>
       )}
     </div>
   );
